c35a1df0e0a3b688504540e986343dcab06ab05f
Radoš <rados.fb@gmail.com>
2025-08-04 19:29:57 +0200

    chore: same formatting

--- src/components/Seat.tsx
+++ src/components/Seat.tsx
@@ -1,7 +1,7 @@
-import { Button } from '@/components/ui/button.tsx';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover.tsx';
-import { cn } from '@/lib/utils.ts';
-import React from 'react';
+import { Button } from "@/components/ui/button.tsx";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover.tsx";
+import { cn } from "@/lib/utils.ts";
+import React from "react";
 
 interface SeatProps extends React.HTMLAttributes<HTMLElement> {}
 
@@ -10,7 +10,7 @@ export const Seat = React.forwardRef<HTMLDivElement, SeatProps>((props, ref) => 
 	return (
 		<Popover>
 			<PopoverTrigger>
-				<div className={cn('size-8 rounded-full bg-zinc-100 hover:bg-zinc-200 transition-color', props.className)} ref={ref}>
+				<div className={cn("size-8 rounded-full bg-zinc-100 hover:bg-zinc-200 transition-color", props.className)} ref={ref}>
 					<span className="text-xs text-zinc-400 font-medium">[n]</span>
 				</div>
 			</PopoverTrigger>
